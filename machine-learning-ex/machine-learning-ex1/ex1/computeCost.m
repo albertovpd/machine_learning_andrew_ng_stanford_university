8f4e7d1bf46c7ec4c4fb29b14d30fb5e9c7387b4
albertovpd <a.vargas.pina@gmail.com>
2020-02-26 21:05:15 +0100

    until feature extraction

--- machine-learning-ex/machine-learning-ex1/ex1/computeCost.m
+++ machine-learning-ex/machine-learning-ex1/ex1/computeCost.m
@@ -13,10 +13,9 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
+J = 1/(2*m)* sum((X*theta - y).^2);
 
-
-
-
+% this is literally the cost function of the notes 
 % =========================================================================
 
 end
